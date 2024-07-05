0c61c486a00f13de5cd809b8154ee09a3dad0cb4
Ethan <125174108+Ethan2Perth@users.noreply.github.com>
2024-07-05 15:06:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'CX',
   BIO: process.env.NEXT_PUBLIC_BIO || 'Attention Is All You Need',
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.cxai.top',
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://cxai.top',
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'MineralS365, 博客',
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   PWA_ENABLE: process.env.NEXT_PUBLIC_PWA_ENABLE || false, // 是否启用 PWA 安装入口；也可在 Notion_Config 中配置 PWA_ENABLE=true
